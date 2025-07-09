83c6d336c9bbea12b9080be5be1e23777ea0e65f
Pratiyush Verma <95582296+pratiyush-alt@users.noreply.github.com>
2025-07-09 10:48:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Get the current Snowflake session
-cnx=st.connection("snowflake")
+cnx = st.connection("snowflake", type="snowflake")
 session = cnx.session()
+
 
 # Get list of available fruits from the Snowflake table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
