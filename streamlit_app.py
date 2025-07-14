75b333d7eec7f0ccf27a6f559663c9d56aa33f3d
Pratiyush Verma <95582296+pratiyush-alt@users.noreply.github.com>
2025-07-14 10:44:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 
 from snowflake.snowpark.functions import col
 
+
+cnx=st.connection("snowflake", type="snowflake")
+
+session = cnx.session()
 # Set Streamlit app title and instructions
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie!")
